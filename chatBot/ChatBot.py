019e7af4402f5bc98f17669fe409f531a35c73b0
Tatoafanasieva <Tamariafanasieva10@gmail.com>
2024-12-26 12:43:18 +0400

    first commit

---
+++ chatBot/ChatBot.py
@@ -0,0 +1,46 @@
+
+name=input("At first please enter your name: ")
+print("Hello " + name)
+
+while True :
+
+  question = input(name+ " " +"let me answer on your questions: ")
+
+  if question =="what costs GOA?":
+    print(" depends on speeIt ds, speed means how many times you have lecture in a week, speed 1 = 1 lecture in a week and costs 170GEL with discount, speed 2 = 2 lecture in a week and costs 290GEL with discount, speed 3 = 3 lecture in a week and costs 390GEL with discount, speed 4 = 4 lecture in a week and costs 490 with discount")
+  elif question == "Why GOA and not, for example, Novator? What is special about you?":
+    print("GOA is the best programming and leadership academy in Georgia and we can say in the world too, it's because we have the best, We have the best approach towards students, we have the best mentors and leaders, if you try it you will be amazed")
+  elif question == "How can I register?":
+    print("At first you have to transfer the amount to the indicated account number This is how you register, and then you have to write to Mr. Nika Keshelava: 1. Link to the child's Facebook profile 2. The child should immediately write to Mr. Nika in your name, with detailed information,3. How many times a week you want to study 4.Tell about the child in detail, age, experience, goals, interests.5. In which stream do you start studying (September/October)")
+  elif question =="what is squads?":  
+      print("there are many leaders who has their squads, The purpose of the leader in the squad is to help the squad in this or that matter, helps the squad to understand everything and at the same time it helps to achieve the goal and settle in Goa")
+  elif question == "can begginers become a leader?":
+    print("it depends on speed if your speed is 3 or 4 u can beacome leader but at first you have to be mini-lader for two months, 1) chad leader, speed 3 (can recruit only speed(1) and speed(2) students, also students from 9 to 13 years old), 1) gigachad leader, speed 4 (can recruit any student in GOA)")
+  elif question == "How long is the lecture?":
+    print("lecture is 2 hours long")
+  elif question == "what does mini-leader do?":
+    print("mini-leader is helping leader im many things like: mini leader is checking students homeworks, and etc.")
+  elif question == "what is hakaton?":
+    print("on hakaton GOA's students are meeting eachother And they compete with each other, unite and create a cozy and pleasant environment")
+  elif question == "How often is a hackathon?":
+    print("May be once in 2-3 months or may be even once in 6 months")
+  elif question == "can student become a mentor?":
+    print("yes students can become a mentor, It is difficult but not impossible")
+  elif question == "bye" :
+    print("bye bye")
+    break
+  else:
+    print("I don't know what are you talking about, can you please tell me again?")
+    
+   
+   
+
+
+
+
+
+
+
+ 
+
+
